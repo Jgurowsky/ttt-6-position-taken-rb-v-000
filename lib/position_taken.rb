36f95aff893fd3535602070b5c9fdbbce3c0de79
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-05-24 08:50:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ def position_taken?(board, index)
   if
     board[index] == "X" or "O"
     return true
-  else board[index] == " " || "" || nil
+  else board[index] == " "
       return false
   end
 end
